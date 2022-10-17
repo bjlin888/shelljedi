60fe0ddbe0f8f2da21b0491e8791c5327560d802
B.J Lin <bingjin.lin@gmail.com>
2022-10-17 16:48:44 +0800

    check memory usage

--- memory/memory_usage.sh
+++ memory/memory_usage.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-ROWS=$( ps -o pid,user,%mem -o comm=Command ax | grep -v PID | sort -bnr -k3 | awk '/[0-9]*/{print $1 ":" $2 ":" $4}' )
-for record in $ROWS
-do
-    echo $record
-    PID=$( echo $record | cut -d: -f1)
-    OWNER=$( echo $record | cut -d: -f2)
-    COMMAND=$( echo $record | cut -d: -f2)
-    MEMORY=$( sudo pmap $PID | tail -n 1 | awk '/[0-9]K/{print $2}')
-    
-    echo "PID: $PID"
-    echo "OWNER: $OWNER"
-    echo "COMMAND: $COMMAND"
-    echo "MEMORY": $MEMORY
-done
+printf "%-10s%-15s%-15s%s\n" "PID" "OWNER" "MEMORY" "COMMAND"
+function main() {
+    ROWS=$( ps -o pid,user,%mem -o comm=Command ax | grep -v PID | sort -bnr -k3 | awk '/[0-9]*/{print $1 ":" $2 ":" $4}' )
+    for record in $ROWS
+    do
+        PID=$( echo $record | cut -d: -f1)
+        OWNER=$( echo $record | cut -d: -f2)
+        COMMAND=$( echo $record | cut -d: -f3)
+        MEMORY=$( sudo pmap $PID | tail -n 1 | awk '/[0-9]/{print $2}')
+        
+        printf "%-10s%-15s%-15s%s\n" "$PID" "$OWNER" "$MEMORY" "$COMMAND"
+    done
+}
+
+main | sort -bnr -k3
